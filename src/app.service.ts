070aa285cca30a57642cbee75dad9cc18b300177
Morice-Gallier Nikola <morice-galliernikola@MacBook-Pro-de-Morice-Gallier.local>
2024-07-06 15:29:42 +0200

    fix/revert to hello world

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
-  getYo(): string {
-    return "Yoooooooo World!";
+  getHello(): string {
+    return "Hello World!";
   }
 }
